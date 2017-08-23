d4281b88bb84b8f321a82417a67fa34e73c230ba
Rob | Labs <roblabs@users.noreply.github.com>
2017-08-23 16:31:14 -0400

    Split CSV into new JS file Upgrade Mapbox GL JS

--- js/index.js
+++ js/index.js
@@ -39,7 +39,6 @@ mapsmall.on("load", function() {
               "text-size": textSize,
               "icon-image": "{icon}-15",
               "text-field": textField,
-              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
               "text-offset": [0, 0.6],
               "text-anchor": "top"
           },
@@ -62,7 +61,6 @@ map.on("load", function() {
               "text-size": textSize,
               "icon-image": "{icon}-15",
               "text-field": textField,
-              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
               "text-offset": [0, 0.6],
               "text-anchor": "top"
           },
